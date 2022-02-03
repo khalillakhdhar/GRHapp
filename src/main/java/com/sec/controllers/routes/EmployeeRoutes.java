c0312516407f364562b9f3cd9f63033fd0d4d93f
khalil <khalillakhdharatc@gmail.com>
2022-02-03 08:56:41 +0100

    Update EmployeeRoutes.java

--- src/main/java/com/sec/controllers/routes/EmployeeRoutes.java
+++ src/main/java/com/sec/controllers/routes/EmployeeRoutes.java
@@ -7,11 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.sec.controllers.services.EmployeeService;
 import com.sec.model.Employee;
 
-@Controller
+@RestController
 public class EmployeeRoutes {
 @Autowired
 EmployeeService api;
@@ -20,7 +21,7 @@ public List<Employee> getAll()
 {
 return api.getEmployee();	
 }
-@PostMapping("employee")
+@PostMapping("/employee")
 public Employee create(@RequestBody Employee employee)
 {
 return api.createUser(employee);	
